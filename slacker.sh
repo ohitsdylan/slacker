5ffbd747282082dbb9409d10a6f82d5beb498c22
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2022-01-19 21:35:38 -0500

    rearrange variables again
    put them in the proper order so they contain the right information
    thanks to @claymager for the assist

--- slacker.sh
+++ slacker.sh
@@ -4,27 +4,30 @@
 # downloads and builds files from slackbuilds.org
 # by ohitsdylan
 
+# Fail if something breaks.
+set -euo pipefail
+
 # Get SlackBuild URL and storage directory.
 read -p 'SlackBuild URL: ' SBURL
 read -p 'Full path to storage directory (i.e. /home/user/directory): ' SBDIR
 read -p 'Project directory (i.e. bar-1.1): ' PRDIR
 
 # Create storage directory and use URL to get files.
-mkdir $SBDIR/$PRDIR
+mkdir -p $SBDIR/$PRDIR
 cd $SBDIR/$PRDIR
 FNAME=$(basename "$SBURL")
 wget $SBURL
+
+# Use tar to decompress files and drop into directory.
+tar -xzf $FNAME
+# Trim the file extension off of the original filename so we can use it somewhere else.
+FNAME=$(echo $FNAME | cut -f 1 -d '.')
+cd $FNAME
 
 # Collect version number.
 VNUM=$(awk -F 'VERSION' '{print $2}' $FNAME.info | cut -f 2 -d '"' | tr -d '\n')
 # Collect download URL
 SBURL=$(awk -F 'DOWNLOAD' '{print $2}' $FNAME.info | cut -f 2 -d '"' | tr -d '\n')
-
-# Use tar to decompress files and drop into directory.
-tar -xzf $FNAME
-FNAME=$(echo $FNAME | cut -f 1 -d '.')
-cd $FNAME
-
 wget $SBURL
 
 # Grant execute rights to the .SlackBuild file and execute.
