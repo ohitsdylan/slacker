b63ed81b67907d5895e90540b6987627874793e4
Dylan <55002846+ohitsdylan@users.noreply.github.com>
2022-01-19 20:28:47 -0500

    get version number more betterer
    do it in a manner similar to the download URL

--- slacker.sh
+++ slacker.sh
@@ -9,28 +9,29 @@ read -p 'SlackBuild URL: ' SBURL
 read -p 'Full path to storage directory (i.e. /home/user/directory): ' SBDIR
 read -p 'Project directory (i.e. bar-1.1): ' PRDIR
 
+# Collect version number.
+VNUM=$(awk -F 'VERSION' '{print $2}' $FNAME.info | cut -f 2 -d '"' | tr -d '\n')
+
+# Collect download URL
+SBURL=$(awk -F 'DOWNLOAD' '{print $2}' $FNAME.info | cut -f 2 -d '"' | tr -d '\n')
+
 # Create storage directory and use URL to get files.
 mkdir $SBDIR/$PRDIR
 cd $SBDIR/$PRDIR
 FNAME=$(basename "$SBURL")
 wget $SBURL
 
-# Collect version number
-VNUM=$(cat $FNAME.info | cut -f 2 -d '"' | sed -n '2 p')
-
 # Use tar to decompress files and drop into directory.
 tar -xzf $FNAME
 FNAME=$(echo $FNAME | cut -f 1 -d '.')
 cd $FNAME
 
-# Grab the source file from the .info file and wget it.
-SBURL=$(awk -F 'DOWNLOAD' '{print $2}' $FNAME.info | cut -f 2 -d '"' | tr -d '\n')
 wget $SBURL
 
 # Grant execute rights to the .SlackBuild file and execute.
 chmod +x $FNAME.SlackBuild
 ./$FNAME.SlackBuild
 
-# Run installpkg and move file into project directory
+# Run installpkg and move file into project directory.
 installpkg /tmp/$FNAME-$VNUM-x86_64-1_SBo.tgz
 mv /tmp/$FNAME-$VNUM-x86_64-1_SBo.tgz .
